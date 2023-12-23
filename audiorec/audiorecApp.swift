1906a24caa425a9f0cdf74f5191a3db901add87c
Joudnaser <joud.naser88@gmail.com>
2023-12-23 21:19:48 +0300

    Initial Commit

---
+++ audiorec/audiorecApp.swift
@@ -0,0 +1,17 @@
+//
+//  audiorecApp.swift
+//  audiorec
+//
+//  Created by joud alkahtany on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct audiorecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ audiorecUITests/audiorecUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  audiorecUITestsLaunchTests.swift
+//  audiorecUITests
+//
+//  Created by joud alkahtany on 23/12/2023.
+//
+
+import XCTest
+
+final class audiorecUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
